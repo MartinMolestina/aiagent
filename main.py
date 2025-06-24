0ef81dc2a446025db15c53aa1ed24a6434eacabf
MartinMolestina <martin.molestina@gmail.com>
2025-06-24 12:44:07 -0500

    message history started

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+
+
+# Load environment variables from .env
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if not api_key:
+    print("Error: GEMINI_API_KEY not set in environment.")
+    sys.exit(1)
+
+# Check for user input
+if len(sys.argv) < 2:
+    print("Usage: python script.py 'your prompt here'")
+    sys.exit(1)
+
+
+user_prompt = sys.argv[1]
+model = "gemini-2.0-flash-001"
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+# Initialize the Gemini client
 client = genai.Client(api_key=api_key)
 
-model = "gemini-2.0-flash-001"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+# Generate the response
+response = client.models.generate_content(model=model, contents=messages)
 
-response = client.models.generate_content(model=model, contents=contents)
+# Print input and response
+print(f"Prompt:\n{contents}\n\nResponse:\n{response.text}\n")
 
-print(f"{contents}\n\n")
-print(response.text)
-
+# Print token usage
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
 print(f"Prompt tokens: {prompt_tokens}")
-print(f"Responde tokens: {response_tokens}")
+print(f"Response tokens: {response_tokens}")
