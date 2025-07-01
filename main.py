8b16dbee0fccf75dd49755a2d33d4d5f4c0d9f53
MartinMolestina <martin.molestina@gmail.com>
2025-07-01 11:53:57 -0500

    function declarations for AI agent

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+
 
 # Load environment variables from .env
 load_dotenv()
@@ -11,6 +14,23 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
     print("Error: GEMINI_API_KEY not set in environment.")
     sys.exit(1)
+
+# Define system prompt for behavior
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+# Define schema for function calling by AI
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info]
+)
 
 # Check for input args
 if len(sys.argv) < 2:
@@ -34,7 +54,26 @@ messages = [
 client = genai.Client(api_key=api_key)
 
 # Generate the response
-response = client.models.generate_content(model=model, contents=messages)
+response = client.models.generate_content(
+    model=model,
+    contents=messages,
+    config=types.GenerateContentConfig(
+        tools=[available_functions],
+        system_instruction=system_prompt
+    )
+)
+
+
+if response.candidates and response.candidates[0].content.parts:
+    part = response.candidates[0].content.parts[0]
+
+    if hasattr(part, "function_call"):
+        print(f"Calling function: {part.function_call.name}({part.function_call.args})")
+    else:
+        print(part.text)
+else:
+    print("No response content received.")
+
 
 # Only print verbose info if --verbose was passed
 if verbose:
